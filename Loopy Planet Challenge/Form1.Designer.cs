d91c17a088f7d643c242293a384d33bd09b22856
harismu24 <harismu24@gmail.com>
2017-02-09 13:37:02 -0500

    Add files via upload

---
+++ Loopy Planet Challenge/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Loopy_Planet_Challenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.cmbPlanet = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(158, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Weight Converter";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select Planet ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(315, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter Your Weight (lbs) ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(32, 336);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(254, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Result Weight (lbs)";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(353, 193);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(119, 26);
+            this.txtWeight.TabIndex = 5;
+            this.txtWeight.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(353, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 43);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(353, 342);
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(119, 26);
+            this.txtResults.TabIndex = 7;
+            // 
+            // cmbPlanet
+            // 
+            this.cmbPlanet.FormattingEnabled = true;
+            this.cmbPlanet.Items.AddRange(new object[] {
+            "Mercury",
+            "Jupiter",
+            "Neptune",
+            "Venus",
+            "Saturn",
+            "Pluto",
+            "Mars",
+            "Uranus"});
+            this.cmbPlanet.Location = new System.Drawing.Point(353, 116);
+            this.cmbPlanet.Name = "cmbPlanet";
+            this.cmbPlanet.Size = new System.Drawing.Size(121, 28);
+            this.cmbPlanet.TabIndex = 8;
+            this.cmbPlanet.Text = "Select One";
+            this.cmbPlanet.SelectedIndexChanged += new System.EventHandler(this.cmbPlanet_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 414);
+            this.Controls.Add(this.cmbPlanet);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.ComboBox cmbPlanet;
+    }
+}
+
